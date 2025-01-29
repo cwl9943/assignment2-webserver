841db7231aee5bee41de234d7cabd89a6b9c774b
cwl9943 <cwl9943@nyu.edu>
2025-01-29 18:32:06 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -34,8 +34,11 @@ def webServer(port=13331):
             #Fill in start 
               
             #Content-Type is an example on how to send a header as bytes. There are more!
-            # outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
-            connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+            outputdata = b"b"HTTP/1.1 200 OK\r\n\Content-Type: text/html; charset=UTF-8\r\n"
+
+            f += outputdata
+            
+#            connectionSocket.send(outputdata)
 
             #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
  
